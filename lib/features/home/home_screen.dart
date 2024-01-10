febc2742ac2984875205f368e424fa43f2e6df9c
0Armaan025 <armaan33000@gmail.com>
2024-01-10 19:29:05 +0530

    connected bar server partially 50% and added js annote

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,8 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:learnflow/utils/pallete.dart';
 import 'package:learnflow/utils/utils.dart';
 import 'package:lottie/lottie.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:js/js.dart' as js;
+import 'package:js/js_util.dart' as js_util;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,29 +18,91 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+@js.JSExport()
 class _HomeScreenState extends State<HomeScreen> {
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = '';
+  final _streamController = 
+  String _textQuery = '';
+
+  List<String> chatHistory = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Pallete().bgColor,
       appBar: buildAppBar(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed:
+            _speechToText.isNotListening ? _startListening : _stopListening,
+        tooltip: 'Listen',
+        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+      ),
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
             children: [
-              const SizedBox(
-                height: 0,
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                width: double.infinity,
-                child: Lottie.asset(
-                  'assets/lottie/hello_animation_1.json',
+              Padding(
+                padding: const EdgeInsets.only(bottom: 48.0),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 260,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                        child: Text(
+                            "Looks like it's the time to study again? 🤔",
+                            style: GoogleFonts.poppins(
+                                color: Pallete().headlineTextColor,
+                                fontSize: 16)),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      width: double.infinity,
+                      child: Lottie.asset(
+                        'assets/lottie/hello_animation.json',
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Text("Looks like it's the time to study again? 🤔",
-                  style:
-                      GoogleFonts.poppins(color: Pallete().headlineTextColor)),
+              Text(
+                // If listening is active show the recognized words
+                _speechToText.isListening
+                    ? '$_lastWords'
+                    : _speechEnabled
+                        ? 'Tap the microphone to start listening...'
+                        : 'Speech not available',
+              ),
             ],
           ),
         ),
