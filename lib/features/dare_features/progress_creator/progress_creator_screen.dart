01156d60225c7b9029aff1582fc95386dcc62ad8
0Armaan025 <armaan33000@gmail.com>
2024-02-15 20:50:58 +0530

    done

---
+++ lib/features/dare_features/progress_creator/progress_creator_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProgressCreatorScreen extends StatefulWidget {
+  const ProgressCreatorScreen({super.key});
+
+  @override
+  State<ProgressCreatorScreen> createState() => _ProgressCreatorScreenState();
+}
+
+class _ProgressCreatorScreenState extends State<ProgressCreatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
